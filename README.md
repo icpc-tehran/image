efbe1a3a779519c143ea50ee85b78c01a8ea0222
Shervin Khastoo <shervinkh145@gmail.com>
2017-11-14 20:49:38 +0330

    Basic changes from IOI to ACM ICPC

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
-IOI 2017 Contest Image Builder
-==============================
+ACM ICPC Regional (Tehran Site) Contest Image Builder
+=====================================================
 
 The Contest Image Builder contains a set of bash scripts used to build
-the contest image for the [IOI 2017](http://ioi2017.org/)
-in Tehran, Iran.
-
+the contest image for the ACM ICPC Regional in Tehran, Iran.
 
 Installation
 ------------
 
-To install the IOI 2017 contest image, follow the instructions below:
+To install the ICPC contest image, follow the instructions below:
 
   * install Ubuntu 16.04 LTS Desktop on your system
-  * create a user named ``ioi2017``
+  * create a user named ``acm``
   * run ``sudo bash install.sh``
 
 Build Live Image
 ----------------
 
-To build a live version of the IOI 2017 contest image,
+To build a live version of the ICPC contest image,
 run ``build.sh`` in the ``live`` directory.
--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Bash script for building the IOI 2017 contest image
-# Version: 1.4
-# http://ioi2017.org/
-
+# Bash script for building the ACM ICPC contest image
+# Version: 0.9
 
 set -xe
 
 if [ -z $LIVE_BUILD ]
 then
-    export USER=ioi2017
+    export USER=acm
     export HOME=/home/$USER
 else
     export USER=root
@@ -29,7 +27,9 @@ EOF
 # Add missing repositories
 add-apt-repository -y ppa:damien-moore/codeblocks-stable
 apt-add-repository -y ppa:mmk2410/intellij-idea
-
+add-apt-repository -y ppa:webupd8team/atom
+echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -sc)-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list
+wget -q -O - http://archive.getdeb.net/getdeb-archive.key | apt-key add -
 
 # Update packages list
 apt-get -y update
@@ -42,21 +42,24 @@ apt-get -y upgrade
 # ----- Install software from Ubuntu repositories -----
 
 # Compilers
-apt-get -y install gcc-5 g++-5 openjdk-8-jdk openjdk-8-source fpc
+apt-get -y install gcc-5 g++-5 openjdk-8-jdk openjdk-8-source
 
 # Editors and IDEs
-apt-get -y install codeblocks codeblocks-contrib emacs geany geany-plugins
-apt-get -y install gedit vim-gnome vim joe kate kdevelop lazarus nano
+apt-get -y install codeblocks codeblocks-contrib emacs geany geany-plugins netbeans
+apt-get -y install gedit vim-gnome vim kate nano
 apt-get -y install intellij-idea-community
+apt-get -y install pycharm
+apt-get -y install atom
+apt-get -y install idle-python2.7 idle-python3.5
 
 # Debuggers
 apt-get -y install ddd libappindicator1 libindicator7 libvte9 valgrind visualvm
 
 # Interpreters
-apt-get -y install python2.7 python3.5 ruby
+apt-get -y install python2.7 python3.5 ruby pypy
 
 # Documentation
-apt-get -y install stl-manual openjdk-8-doc fp-docs python2.7-doc python3.5-doc
+apt-get -y install stl-manual openjdk-8-doc python2.7-doc python3.5-doc pypy-doc
 
 # Other Software
 apt-get -y install firefox konsole mc
@@ -80,12 +83,13 @@ su $USER -c "mkdir -p $HOME/.config/Code/User"
 su $USER -c "mkdir -p $HOME/.vscode/extensions"
 su $USER -c "HOME=$HOME code --user-data-dir=$HOME/.config/Code/ --install-extension ms-vscode.cpptools"
 su $USER -c "HOME=$HOME code --user-data-dir=$HOME/.config/Code/ --install-extension georgewfraser.vscode-javac"
+su $USER -c "HOME=$HOME code --user-data-dir=$HOME/.config/Code/ --install-extension magicstack.MagicPython"
 
-# Eclipse 4.6 and CDT plugins
-wget http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/neon/2/eclipse-java-neon-2-linux-gtk-x86_64.tar.gz
-tar xzvf eclipse-java-neon-2-linux-gtk-x86_64.tar.gz -C /opt/
-mv /opt/eclipse /opt/eclipse-4.6
-/opt/eclipse-4.6/eclipse -application org.eclipse.equinox.p2.director -noSplash -repository http://download.eclipse.org/releases/neon \
+# Eclipse 4.7 and CDT plugins
+wget http://eclipse.mirror.rafal.ca/technology/epp/downloads/release/oxygen/R/eclipse-java-oxygen-R-linux-gtk-x86_64.tar.gz
+tar xzvf eclipse-java-oxygen-R-linux-gtk-x86_64.tar.gz -C /opt/
+mv /opt/eclipse /opt/eclipse-4.7
+/opt/eclipse-4.7/eclipse -application org.eclipse.equinox.p2.director -noSplash -repository http://download.eclipse.org/releases/oxygen \
 -installIUs \
 org.eclipse.cdt.feature.group,\
 org.eclipse.cdt.build.crossgcc.feature.group,\
@@ -99,8 +103,9 @@ org.eclipse.cdt.autotools.feature.group,\
 org.eclipse.linuxtools.valgrind.feature.group,\
 org.eclipse.linuxtools.profiling.feature.group,\
 org.eclipse.remote.core,\
-org.eclipse.remote.feature.group
-ln -s /opt/eclipse-4.6/eclipse /usr/bin/eclipse
+org.eclipse.remote.feature.group,\
+org.python.pydev.feature.feature.group
+ln -s /opt/eclipse-4.7/eclipse /usr/bin/eclipse
 
 # Sublime Text 3
 wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
@@ -109,13 +114,7 @@ dpkg -i sublime-text_build-3126_amd64.deb
 wget http://ioi2017.org/files/htc/C++.sublime-package
 mv C++.sublime-package /opt/sublime_text/Packages
 
-# AltGr
-mkdir -p /usr/local/share/altgr/
-wget http://ioi2017.org/files/htc/icon.png -O /usr/local/share/altgr/icon.png
-wget http://ioi2017.org/files/htc/enable_altgr.sh -O /opt/enable_altgr.sh
-wget http://ioi2017.org/files/htc/disable_altgr.sh -O /opt/disable_altgr.sh
 chmod +x /opt/*.sh
-
 
 # ----- Create desktop entries -----
 
@@ -148,32 +147,10 @@ cat << EOF > eclipse.desktop
 Type=Application
 Name=Eclipse Neon
 Comment=Eclipse Integrated Development Environment
-Icon=/opt/eclipse-4.6/icon.xpm
+Icon=/opt/eclipse-4.7/icon.xpm
 Exec=eclipse
 Terminal=false
 Categories=Development;IDE;Java;
-EOF
-
-cat << EOF > disable_altgr.desktop
-[Desktop Entry]
-Type=Application
-Name=Disable Menu\non Alt Gr
-Comment=Disable AltGr
-Exec=/opt/disable_altgr.sh
-Icon=/usr/local/share/altgr/icon.png
-Terminal=true
-Categories=AltGr;
-EOF
-
-cat << EOF > enable_altgr.desktop
-[Desktop Entry]
-Type=Application
-Name=Enable Menu\non Alt Gr
-Comment=Enable AltGr
-Exec=/opt/enable_altgr.sh
-Icon=/usr/local/share/altgr/icon.png
-Terminal=true
-Categories=AltGr;
 EOF
 
 cat << EOF > cpp-doc.desktop
@@ -185,17 +162,6 @@ Icon=firefox
 Exec=firefox /opt/cppref/reference/en/index.html
 Terminal=false
 Categories=Documentation;C++;
-EOF
-
-cat << EOF > fp-doc.desktop
-[Desktop Entry]
-Type=Application
-Name=FreePascal Documentation
-Comment=FreePascal Documentation
-Icon=firefox
-Exec=firefox /usr/share/doc/fp-docs/3.0.0/fpctoc.html
-Terminal=false
-Categories=Documentation;FP;
 EOF
 
 cat << EOF > java-doc.desktop
@@ -229,19 +195,19 @@ chown $USER "$HOME/Desktop/Utils"
 chown $USER "$HOME/Desktop/Docs"
 
 # Copy Editors and IDEs
-for i in gedit codeblocks emacs24 geany lazarus-1.6 org.kde.kate sublime_text eclipse code vim gvim kde4/kdevelop intellij-idea-community
+for i in gedit codeblocks emacs24 geany org.kde.kate sublime_text eclipse code vim gvim intellij-idea-community
 do
     cp "$i.desktop" "$HOME/Desktop/Editors & IDEs"
 done
 
 # Copy Docs
-for i in cpp-doc stl-manual java-doc fp-doc python2.7-doc python3.5-doc
+for i in cpp-doc stl-manual java-doc python2.7-doc python3.5-doc
 do
     cp "$i.desktop" "$HOME/Desktop/Docs"
 done
 
 # Copy Utils
-for i in ddd disable_altgr enable_altgr gnome-calculator gnome-terminal mc org.kde.konsole visualvm
+for i in ddd gnome-calculator gnome-terminal mc org.kde.konsole visualvm
 do
     cp "$i.desktop" "$HOME/Desktop/Utils"
 done
@@ -255,7 +221,7 @@ chmod a+x "$HOME/Desktop/Docs"/*
 apt-get install -y xvfb
 if [ -z $LIVE_BUILD ]
 then
-    wget -O /opt/wallpaper.png "http://ioi2017.org/files/htc/wallpaper.png"
+    cp live/files/wallpaper.png /opt/wallpaper.png
 fi
 xvfb-run gsettings set org.gnome.desktop.background primary-color "#000000000000"
 xvfb-run gsettings set org.gnome.desktop.background picture-options "spanned"
